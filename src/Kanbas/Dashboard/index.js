167b7750fc01a4fe5ad96444cc166a84b620ccb8
rgarcia101 <raquel_garcia96@yahoo.com>
2023-10-21 13:26:38 -0400

    styled dashboard

--- src/Kanbas/Dashboard/index.js
+++ src/Kanbas/Dashboard/index.js
@@ -6,19 +6,20 @@ function Dashboard(  { courses, course, setCourse, addNewCourse,
     deleteCourse, updateCourse }) {
 
   return (
-    <div className="page-margin-left page-margin-right">
+    <div className="page-margin-left page-margin-right button-margin-bottom">
       <h1>Dashboard</h1>
       <hr/>
       <h2>Published ({courses.length})</h2>
+      <hr/>
       <h5>Course</h5>
-      <input value={course.name} className="form-control" onChange={(e) => setCourse({ ...course, name: e.target.value }) }/>
-      <input value={course.number} className="form-control" onChange={(e) => setCourse({ ...course, number: e.target.value }) } />
-      <input value={course.startDate} className="form-control" type="date" onChange={(e) => setCourse({ ...course, startDate: e.target.value }) } />
-      <input value={course.endDate} className="form-control" type="date" onChange={(e) => setCourse({ ...course, endDate: e.target.value }) }/>
-      <button className="button-margin" onClick={addNewCourse} >
+      <input value={course.name} className="form-control w-25 button-margin-top-bottom" onChange={(e) => setCourse({ ...course, name: e.target.value }) }/>
+      <input value={course.number} className="form-control w-25 button-margin-top-bottom" onChange={(e) => setCourse({ ...course, number: e.target.value }) } />
+      <input value={course.startDate} className="form-control w-25 button-margin-top-bottom" type="date" onChange={(e) => setCourse({ ...course, startDate: e.target.value }) } />
+      <input value={course.endDate} className="form-control w-25 button-margin-top" type="date" onChange={(e) => setCourse({ ...course, endDate: e.target.value }) }/>
+      <button className="button-margin button-margin-top button-margin-bottom btn btn-success" onClick={addNewCourse} >
         Add
       </button>
-      <button onClick={updateCourse} >
+      <button className="button-margin-top button-margin-bottom btn btn-primary" onClick={updateCourse} >
         Update
       </button>
 
@@ -27,7 +28,7 @@ function Dashboard(  { courses, course, setCourse, addNewCourse,
           <Link key={course._id} to={`/Kanbas/Courses/${course._id}`} className="card h-100 card-style">
             <img src="../../images/starship.jpg" className="card-img-top" alt="..." />
             <div className="card-body">
-            <button className="button-margin"
+            <button className="button-margin btn btn-primary button-margin-bottom"
               onClick={(event) => {
                 event.preventDefault();
                 setCourse(course);
@@ -35,7 +36,7 @@ function Dashboard(  { courses, course, setCourse, addNewCourse,
               Edit
             </button>
 
-              <button
+              <button className="btn btn-danger button-margin-bottom"
                 onClick={(event) => {
                 event.preventDefault();
                 deleteCourse(course._id);
